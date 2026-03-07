dd438b84a836dda1b04bf1a976402610ca0d9d45
Drramos153-pixel <drramos153@gmail.com>
2026-03-07 14:09:28 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,33 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+import os
 
-TOKEN = "8641737018:AAF6DXmD_EIS1FWHNRIgmYnul4VXlPD0zb0"
+TOKEN = os.getenv("TOKEN")
+CANAL_LINK = "https://t.me/+LcqMJ8HuoUxiYjU5"
 
-async def start(update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot funcionando!")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("🔞 Acessar Conteúdo VIP", callback_data="acesso")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "🔥 Bem-vindo ao conteúdo VIP.\n\nClique abaixo para acessar:",
+        reply_markup=reply_markup
+    )
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "acesso":
+        await query.message.reply_text(
+            f"🔞 Entre no canal privado:\n{CANAL_LINK}"
+        )
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(button))
 
 print("Bot iniciado...")
 app.run_polling()
