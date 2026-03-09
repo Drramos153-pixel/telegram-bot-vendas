daff27ef3adab818abd3ddb6e9b738803d68f7e7
Drramos153-pixel <drramos153@gmail.com>
2026-03-08 21:56:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import asyncio
 from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -12,16 +13,24 @@ app = Flask(__name__)
 def home():
     return "Servidor online"
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot funcionando!")
 
-def run_bot():
+
+async def run_bot():
     tg_app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     tg_app.add_handler(CommandHandler("start", start))
+
     print("Bot iniciado")
-    tg_app.run_polling()
+    await tg_app.run_polling()
+
+
+def start_bot():
+    asyncio.run(run_bot())
+
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot, daemon=True).start()
+    threading.Thread(target=start_bot).start()
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
